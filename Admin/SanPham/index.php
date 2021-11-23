c23a9adebd635210efe300b34bd09b4d6d0b7116
c3katsu3c <thangyb2706@gmail.com>
2021-11-23 16:24:09 +0700

    PĐT up load SP, Dòng SP bên Admin

--- Admin/SanPham/index.php
+++ Admin/SanPham/index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Frontend//css/SanPham.css?version=1">
+    <link rel="stylesheet" href="../Frontend//css/SanPham.css?version=2">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
