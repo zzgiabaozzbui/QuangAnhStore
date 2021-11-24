5fffb91e748a5dd942eed0ccc1b0a99b6b6dcef8
zzgiabaozzbui <zzgiabaozzbui@gmail.com>
2021-11-24 23:06:51 +0700

    qb

--- Admin/Home/index.php
+++ Admin/Home/index.php
@@ -14,6 +14,83 @@
     <script type="text/javascript" src="jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
     <script src="home.js"></script>
+    <script>
+        
+
+//Biểu đồ thống kê
+
+$(document).ready(function () {
+    show();
+});
+
+function show(){
+
+    $.post("data.php",
+        function (data){
+            console.log(data);
+            //Tên cột
+            var formStatusVar = [];
+            //Chiều cao cột
+            var total = []; 
+            var total2 = []; 
+
+            for (var i in data) {
+                formStatusVar.push(data[i].Quyen);
+                total.push(data[i].size_status);
+                total2.push(data[i].size_status);
+            }
+
+            var options = {
+                legend: {
+                    //Ẩn nhãn biểu đồ thì dùng false
+                    display: true
+                },
+                scales: {
+                    xAxes: [{
+                        //chiều rộng cột
+                        barPercentage: 0.5,
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                },
+                
+            };
+
+            var myChart = {
+                labels: formStatusVar,
+                datasets: [
+                    {
+                        label: 'Thống kê điện thoại',
+                        backgroundColor: '#17cbd1',
+                        borderColor: '#46d5f1',
+                        hoverBackgroundColor: '#0ec2b6',
+                        hoverBorderColor: '#42f5ef',
+                        data: total
+                    },
+                    {
+                        label: 'Thống kê phụ kiện',
+                        backgroundColor: '#FE0066',
+                        borderColor: '#FE0066',
+                        hoverBackgroundColor: '#FE0098',
+                        hoverBorderColor: '#FE0098',
+                        data: total2
+                    }
+                ]
+            };
+
+            var bar = $("#graph"); 
+            var barGraph = new Chart(bar, {
+                type: 'bar',
+                data: myChart,
+                options: options
+            });
+        });
+}
+    </script>
+
 
     <link rel="stylesheet" href="../../Frontend/css/sideBar.css?version=1">
     <link rel="stylesheet" href="../../Frontend/font/themify-icons/themify-icons.css">
