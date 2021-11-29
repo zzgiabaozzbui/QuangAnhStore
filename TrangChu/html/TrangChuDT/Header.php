8e017f1c6d23c276fbd3dcae8ff652fc08c9e58c
NinhMon2K <nguyennghianinh98@gmail.com>
2021-11-29 20:59:14 +0700

    Ninh 21:02

--- TrangChu/html/TrangChuDT/Header.php
+++ TrangChu/html/TrangChuDT/Header.php
@@ -9,9 +9,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://localhost/QuangAnhStore/TrangChu/font/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="http://localhost/QuangAnhStore/TrangChu/css/HeaderStyle.css">
 
 
-    <link rel="stylesheet" href="../../css/HeaderStyle.css">
     <!-- <link rel="stylesheet" href="../css/tablet.css"> -->
     <title>Document</title>
 </head>
